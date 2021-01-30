6f76b0b98238b82328a2851cc0a3a8f374d9e5a5
julius turunen <julius.turunen@aalto.fi>
2021-01-30 05:15:44 +0200

    Refactor exports

---
+++ client/src/contact/Contact.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ContactForm from './ContactForm';
+
+export default () => {
+  return (
+    <div id='contact'>
+      <ContactForm />
+    </div>
+  )
+}
--- client/src/contact/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ContactForm from './ContactForm';
-
-export default () => (
-  <div id='contact'>
-    <ContactForm />
-  </div>
-);

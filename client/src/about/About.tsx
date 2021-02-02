bd10c41912d2cbb5720528aa6a669b118886c267
julius turunen <julius.turunen@aalto.fi>
2021-02-02 05:43:49 +0200

    Update about component and default link colors

--- client/src/about/About.tsx
+++ client/src/about/About.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default () => {
   return (
     <div id='about'>
       <p>
-        My name is Julius Turunen and I am a Computer Science student at 
-        Aalto University. 
+        My name is Julius Turunen, I am a Computer Science student at 
+        Aalto University, on the verge of graduation. 
+      </p>
+      <p>
+        I am interested in web development and I have experience with
+        TypeScript and React. However, I am very motivated to learn
+        new technologies, such as WebAssembly more recently. To get a
+        better picture, you can take a look at my <Link to='/projects'>
+        projects</Link> to see what I have been working on recently.
+      </p>
+      <p>
+        My GitHub is <a href='https://github.com/julleboi'>
+        github.com/julleboi</a>.
       </p>
     </div>
   );
